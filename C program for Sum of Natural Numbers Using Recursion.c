04afff1fb4675dcfeab2cec44c85f7466af5a8e8
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-10-16 21:22:09 +0530

    Update and rename C program for Sum of Natural Numbers Using Recursion to C program for Sum of Natural Numbers Using Recursion.c

--- C program for Sum of Natural Numbers Using Recursion.c
+++ C program for Sum of Natural Numbers Using Recursion.c
@@ -1,31 +1,18 @@
 #include <stdio.h>
 int addnum(int n);
-
 int main()
-
 {
-
+  printf("RA2211042010050");
   int num;
-
   scanf("%d",&num);
-
   printf("%d",addnum(num));
-
   return 0;
-
 }
-
 int addnum(int n)
-
 {
-
   if(n!=0)
-
     return n+addnum(n-1);
-
   else
-
     return n;
-
 }
 
