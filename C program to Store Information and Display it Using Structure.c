de0d102e42af0c6a44726d9a7642ea95c6150d44
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-10-16 21:23:26 +0530

    Update and rename C program to Store Information and Display it Using Structure to C program to Store Information and Display it Using Structure.c

--- C program to Store Information and Display it Using Structure.c
+++ C program to Store Information and Display it Using Structure.c
@@ -1,34 +1,19 @@
 #include <stdio.h>
 int main()
-
 {
-
   struct student
-
   {
-
+    printf("RA2211042010050");
     char name[80];
-
     int rollno;
-
     float marks;
-
   };
-
   struct student s;
-
   scanf("%s",s.name);
-
   scanf("%d",&s.rollno);
-
   scanf("%f",&s.marks);
-
   printf("Name=%s\n",s.name);
-
-  printf("Roll number=%d\n",s.rollno);
-
+  printf("Roll no=%d\n",s.roll);
   printf("Marks=%.1f\n",s.marks);
-
   return 0;
-
 }
