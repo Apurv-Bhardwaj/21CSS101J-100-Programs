fff336c4fc48dc7036a7ab7ddb6d164f62faeb64
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-10-16 21:27:59 +0530

    Update and rename C program to find area of a triangle to C program to find area of a triangle.c

--- C program to find area of a triangle.c
+++ C program to find area of a triangle.c
@@ -2,16 +2,12 @@
 #include <math.h>
 int main()
 {
+  printf("RA2211042010050");
   double a, b, c, s, area;
-
   printf("Enter sides of a triangle\n");
   scanf("%lf%lf%lf", &a, &b, &c);
-
   s = (a+b+c)/2; // Semiperimeter
-
   area = sqrt(s*(s-a)*(s-b)*(s-c));
-
   printf("Area of the triangle = %.2lf\n", area);
-
   return 0;
 }
