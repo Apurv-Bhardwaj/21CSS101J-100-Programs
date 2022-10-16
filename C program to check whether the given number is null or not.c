aa6c2813995434790c80e527ee72059be2291011
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-10-16 21:24:03 +0530

    Update and rename C program to check whether the given number is null or not to C program to check whether the given number is null or not.c

--- C program to check whether the given number is null or not.c
+++ C program to check whether the given number is null or not.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
 int main()
-
 {
-
+  printf("RA2211042010050");
   int a;
-
   scanf("%d",&a);
-
   if(a==0)
-
     printf("NULL");
-
   else
-
     printf("NOT NULL");
  return 0;
-
 }
