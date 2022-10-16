36a65927db764b9b6a2dde18ef8600b912adfcf9
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-10-16 21:25:29 +0530

    Update and rename C program to count frequency of each character in a string using loop to C program to count frequency of each character in a string using loop.c

--- C program to count frequency of each character in a string using loop.c
+++ C program to count frequency of each character in a string using loop.c
@@ -1,46 +1,25 @@
 #include <stdio.h>
 #include<string.h>
-
 int main()
-
 {
-
+  printf("RA2211042010050");
   char str[100];
-
   int c=0;
-
   int count[26]={0},x;
-
   scanf("%s",str);
-
   while(str[c]!='\0')
-
   {
-
     if(str[c]>='a'&&str[c]<='z')
-
     {
-
       x=str[c]-'a';
-
       count[x]++;
-
     }
-
 c++;
-
 }
-
 for(c=0;c<26;c++)
-
 {
-
   if(count[c]!=0)
-
   printf("%c = %d\n",c+'a',count[c]);
-
 }
-
  return 0;
-
 }
