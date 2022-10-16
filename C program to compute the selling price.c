29e3275c4b3fff398cf7b9c89da958ee6b65ed81
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-10-16 21:24:43 +0530

    Update and rename C program to compute the selling price to C program to compute the selling price.c

--- C program to compute the selling price.c
+++ C program to compute the selling price.c
@@ -1,34 +1,19 @@
 #include<stdio.h>
 int main()
-
 {
-
+printf("RA2211042010050");
   int a,b;
-
   float loss,tot;
-
   scanf("%d%d",&a,&b);
-
   if(a==0)
-
   {
-
     printf("%d",a);
-
   }
-
   else
-
   {
-
   loss=a*(b*0.01);
-
   tot=a-loss;
-
   printf("The selling price of the cycle is Rs=%.2f",tot);
-
   }
-
   return 0;
-
 }
