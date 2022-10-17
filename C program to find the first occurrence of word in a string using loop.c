3f49d4941438893e7908e7178e45b3444bd5c247
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-10-17 16:05:26 +0530

    Update C program to find the first occurrence of word in a string using loop.c

--- C program to find the first occurrence of word in a string using loop.c
+++ C program to find the first occurrence of word in a string using loop.c
@@ -1,92 +1,33 @@
 #include <stdio.h>
 #include <string.h>
-
 #define MAX_SIZE 100
-
-
-
-
 int main()
-
 {
-
+printf("RA2211042010050");
     char str[MAX_SIZE];
-
     char word[MAX_SIZE];
-
     int i, j, found;
-
     int strLen, wordLen;
-
-
-
-
-
-
-
     scanf("%[^\n]%*c",str);
-
-    scanf("%s",word);
-
-
-
-
-    strLen  = strlen(str);  
-
+   scanf("%s",word);
+    strLen  = strlen(str);  ]
     wordLen = strlen(word); 
-
-
-
-
-
-
-
     for(i=0; i<strLen; i++)
-
     {
-
-
-
-
         found = 1;
-
         for(j=0; j<wordLen; j++)
-
         {
-
-       
-
             if(str[i + j] != word[j])
-
             {
-
-                found = 0;
+             found = 0;
 
                 break;
-
             }
-
         }
-
-
-
-
-
-
-
         if(found == 1)
-
         {
-
-            printf("%s is found at %d\n",word,i);
-
+          printf("%s is found at %d\n",word,i);
         }
-
     }
-
-
-
-
     return 0;
-
 }
