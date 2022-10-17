8d39afee3e5b6d1d46685db3afd8509ceb1691c4
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-10-17 23:32:12 +0530

    Update and rename C program to swap elements in cyclic order using call by reference to C program to swap elements in cyclic order using call by reference.c

--- C program to swap elements in cyclic order using call by reference.c
+++ C program to swap elements in cyclic order using call by reference.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
 int main()
-
 {
-
+  printf("RA2211042010050");
   int a,b,c;
-
   scanf("%d%d%d",&a,&b,&c);
-
   printf("%d",c);
-
   printf("\n%d",a);
-
   printf("\n%d",b);
-  
  return 0;
-
 }
