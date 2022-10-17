524c7c5a9953bc4323659a1f9ae4bbdd59fb6d4f
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-10-17 23:47:07 +0530

    Update and rename Creating two structs to Creating two structs.c

--- Creating two structs.c
+++ Creating two structs.c
@@ -1,28 +1,16 @@
 #include<stdio.h>
 struct student
-
 {
-
+printf("RA2211042010050");
  char name[100];
-
 struct dateOfBirth
-
 {
-
   int rollno,date,month,year;
-
 }DOB;
-
 }std;
-
 int main()
-
 {
-
   scanf("%s%d%d%d%d",std.name,&std.DOB.rollno,&std.DOB.date,&std.DOB.month,&std.DOB.year);
-
   printf("Name=%s\nRollNo=%d\nDate of birth=%d/%d/%d",std.name,std.DOB.rollno,std.DOB.date,std.DOB.month,std.DOB.year);
-
   return 0;
-
 }
