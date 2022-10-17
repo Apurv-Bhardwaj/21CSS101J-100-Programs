edb966729280145d95dc0b42953f61f94f065c2a
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-10-17 23:32:57 +0530

    Update and rename Code to display Largest Element of an array to Code to display Largest Element of an array.c

--- Code to display Largest Element of an array.c
+++ Code to display Largest Element of an array.c
@@ -2,49 +2,25 @@
 int main()
 
 {
-
+printf("RA2211042010050");
   int a[10],n,j,i,temp;
-
   scanf("%d",&n);
-
   for(i=0;i<n;i++)
-
     {
-
     scanf("%d",&a[i]);
-
     }
-
   for(i=0;i<n;i++)
-
     {
-
     for (j=0;j<n;j++)
-
       {
-
       if(a[i]<a[j])
-
         {
-
         a[i]=temp;
-
         a[i]=a[j];
-
         temp=a[j];
-
       }
-
-    }
-
-    
-
-  }
-
+    }  
+ }
   printf ("%d",a[n-1]);
-
- 
-
  return 0;
-
 }
