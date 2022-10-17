fcc6883f9459727a0fba4b7ca8d0438c67aca3c6
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-10-17 16:06:56 +0530

    Update and rename C program to find whether a given matrix is a lower triangular matrix or not to C program to find whether a given matrix is a lower triangular matrix or not.c

--- C program to find whether a given matrix is a lower triangular matrix or not.c
+++ C program to find whether a given matrix is a lower triangular matrix or not.c
@@ -1,58 +1,31 @@
 #include <stdio.h>
 #include<math.h>
-
 int main()
-
 {
-
+printf("RA2211042010050");
   int i,j,x,y,flag=0;
-
   scanf("%d%d",&x,&y);
-
   int a[x][y];
-
   for(i=0;i<x;i++)
-
   {
-
     for(j=0;j<y;j++)
-
     {
-
       scanf("%d",&a[i][j]);
-
     }
-
   }
-
   for(i=0;i<x;i++)
-
   {
-
     for(j=i+1;j<y;j++)
-
     {
-
       if(a[i][j]!=0)
-
       {
-
         flag=1;
-
       }
-
     }
-
   }
-
   if(flag==0)
-
     printf("yes");
-
   else
-
     printf("no");
-
   return 0;
-
 }
