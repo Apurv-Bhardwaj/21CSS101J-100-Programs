1cbebad1c820ee07949058b38f7cc45a15c4f585
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-10-17 23:35:18 +0530

    Update and rename Code to find the sum of factors of a given number to Code to find the sum of factors of a given number.c

--- Code to find the sum of factors of a given number.c
+++ Code to find the sum of factors of a given number.c
@@ -1,39 +1,20 @@
 #include <stdio.h>
-
 int main()
-
 {
-
+printf("RA2211042010050");
   int sum,n=1,a;
-
   scanf("%d",&a);
-
   if(a==0)
-
-    printf("0");
-
+   printf("0");
   while(n<=a)
-
   {
-
     if(a!=0 && a%n==0)
-
     {
-
       printf("%d ",n);
-
       sum=sum+n;
-
     }
-
     n=n+1;
-
   }
-
   printf("\nSum=%d",sum);
-
- 
-
             return 0;
-
 }
