4052de06d52bce22516ccb8e382813c848e2fa7d
Apurv-Bhardwaj <115153267+Apurv-Bhardwaj@users.noreply.github.com>
2022-10-17 23:33:48 +0530

    Update and rename Code to find the prime number between 2 and user entered number to Code to find the prime number between 2 and user entered number.c

--- Code to find the prime number between 2 and user entered number.c
+++ Code to find the prime number between 2 and user entered number.c
@@ -1,37 +1,19 @@
 #include <stdio.h>
 int main()
-
 {
-
+  printf("RA2211042010050");
   int n,i,fact,j;
-
   scanf("%d",&n);
-
   for(i=3;i<n;i++)
-
   {
-
     fact=0;
-
     for(j=1;j<n;j++)
-
     {
-
       if(i%j==0)
-
         fact++;
-
     }
-
     if(fact==2)
-
-      printf("%d ",i);
-
+      printf("%d ",i);]
   }
-
-
-
-
  return 0;
-
 }
